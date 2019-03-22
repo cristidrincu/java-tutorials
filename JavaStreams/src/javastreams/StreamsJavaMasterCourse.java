f78d7942d36f4216ae5faf7d4042d13a838343d5
Cristian Sergiu Drincu <cristidrincu@gmail.com>
2019-03-22 08:00:42 +0200

    Understand streams intermediary and terminal operations. Understand flat map.

--- JavaStreams/src/javastreams/StreamsJavaMasterCourse.java
+++ JavaStreams/src/javastreams/StreamsJavaMasterCourse.java
@@ -1,9 +1,13 @@
 package javastreams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import streamsflatmap.Department;
+import streamsflatmap.Employee;
 
 /**
  *
@@ -39,6 +43,48 @@ public class StreamsJavaMasterCourse {
                 .distinct() //no duplicates in concat stream - distinct() elements - read more on final operations vs intermediate operations in streams
                 .peek(System.out::println)
                 .count()); 
+        
+        
+            //flat map
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Deer", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee christian = new Employee("Cristian Drincu", 30);
+        Employee snow = new Employee("Snow White", 22);
+
+        Department hr = new Department("Human Resources");
+        Department marketing = new Department("Marketing");
+        
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(christian);
+        hr.addEmployee(snow);
+        marketing.addEmployee(john);
+        
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(marketing);
+        
+        //stream operations like filter, map etc are lazily evaluated, they are called intermediate operations
+        //- this means that they will not be executed until a terminal operation is appended to the pipe -
+        //a terminal operation is collect, reduce.
+        //flat nested lists contained in an object - in our case, the object containing the lists is department which has a list of employees
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
+        
+        //grouping by criteria
+        Map<Integer, List<Employee>> groupedByAge = departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .collect(Collectors.groupingBy(employee -> employee.getAge()));
+        
+        System.out.println("Grouped by age: " + groupedByAge.toString());
+        
+        //using the reduce terminal operation
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+        
     }
-    
 }
